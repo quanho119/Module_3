a6e0c9805d6a667e0cc3fa2ce8de49207b351075
MinhWaan <hoquan119@gmail.com>
2024-07-24 19:41:04 +0700

    24/7

---
+++ 9-WebIntroduce/Ex/ServletPractise/src/main/java/org/example/servletpractise/ConverterServlet.java
@@ -0,0 +1,39 @@
+package org.example.servletpractise;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ConverterServlet", value = "/convert")
+public class ConverterServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        float rate = Float.parseFloat(request.getParameter("rate"));
+        float usd = Float.parseFloat(request.getParameter("usd"));
+
+        float vnd = rate * usd;
+        PrintWriter writer = response.getWriter();
+        response.setContentType("text/html");
+        writer.println("<html>");
+        writer.println("<h1>Rate: " + rate + "</h1>");
+        writer.println("<h1>USD: " + usd + "</h1>");
+        writer.println("<h1>VND: " + vnd + "</h1>");
+        writer.println("</html>");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
